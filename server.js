5c4b61f279b1b705d0650ea274cebdf4ef0d3421
jihen2021 <tun.desc3@gmail.com>
2021-10-03 10:18:03 +0100

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const connectdb=require('./config/connectdb')
+require("dotenv").config()
+app.use(express.json())
+connectdb()
+app.use("/api",require("./routes/user"))
+
+app.listen(port, (erreur) => erreur? console.log(erreur): console.log(`server is running at port ${port}`))
